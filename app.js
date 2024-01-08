51994438d9ec83b9cc42a9ea5fb5df5813813c80
Zainabfarah <zainabfarah890@gmail.com>
2024-01-08 18:29:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+const PORT = process.env.PORT  || 8000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get('/test', (req, res) => {
+    res.send('Test route works!');
+  });
